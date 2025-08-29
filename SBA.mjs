28c21d031f76cf041d1485130a7fd80020a37a95
Tracey-goode <Jerand322@gmail.com>
2025-08-29 14:00:15 -0400

    second commit

--- SBA.mjs
+++ SBA.mjs
@@ -1,6 +1,13 @@
 async function fetchMarvelCharacters() {
-      try {
+    try {
         const response = await fetch('/api/marvel');
-    const data = await response.json();
-    const container = document.getElementById('results');
-    container.innerHTML = '';
+        const data = await response.json();
+        const container = document.getElementById('results');
+        container.innerHTML = '';
+        data.data.results.forEach(character => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+        <h3>${character.name}</h3>
+        <img src="${character.thumbnail.path}.${character.thumbnail.extension}" width="100">`;
+            container.appendChild(div);
+        });
